f0a0085be14d2d5c557ca12f1c5ab5ec780740d8
Wayne Warner <wawjr3d@gmail.com>
2013-07-12 11:55:47 -0400

    use key up/down to change smiley state for all inputs

--- js/smileys.js
+++ js/smileys.js
@@ -1,55 +1,55 @@
 (function(global, $, undefined) {
-	"use strict";
-	
-	var AVAILABLE_MOODS = ["normal", "elated", "happy", "frowny"].join(" "),
-		
-		AVAILABLE_SPECIFICS = ["normal", "inquisitive", "lying", "honest", "angry", "constipated"].join(" "),
-		
-		ALL_SMILEYS = [AVAILABLE_MOODS, AVAILABLE_SPECIFICS].join(" "),
-	
-		DEFAULT_SIZE = "2",
-		
-		SIZE_UNITS = "px",
-		
-		MAX_SIZE = 40,
-		MIN_SIZE = 1,
-			
-    	// keys i care about
-		KEYUP = 38,
-		KEYDOWN = 40,
-		ENTER_KEY = 13,
-		
-		FIELDS = {
-			SIZE: "size",
-			MOOD: "mood",
-			SPECIFIC: "specific"
-		},
-		
-		TYPE = "{{type}}",
-		SIZE = "{{size}}",
-		
-		smileyLink,
-		
-		HTML = ["<div class='" + TYPE + "'>",
-					"\t<div class='left eye'></div>",
-					"\t<div class='right eye'></div>",
-					"\t<div class='mouth'></div>",
-				"</div>"].join("\n"),
-				
-		CSS = ["<style type='text/css'>",
-		       		"\t.smiley {",
-		       		"\t\tfont-size: " + SIZE + "px;",
-		       		"\t}",
-		       "</style>"].join("\n");
-	
-	
-	var $form, $smiley, $size, $cssCode, $htmlCode;
-	
-	String.prototype.escapeHTML = function () {                                                                                                        
-		return this.replace(/>/g,'&gt;')
-					.replace(/</g,'&lt;')
-					.replace(/"/g,'&quot;');
-	};
+    "use strict";
+    
+    var AVAILABLE_MOODS = ["normal", "elated", "happy", "frowny"].join(" "),
+        
+        AVAILABLE_SPECIFICS = ["normal", "inquisitive", "lying", "honest", "angry", "constipated"].join(" "),
+        
+        ALL_SMILEYS = [AVAILABLE_MOODS, AVAILABLE_SPECIFICS].join(" "),
+    
+        DEFAULT_SIZE = "2",
+        
+        SIZE_UNITS = "px",
+        
+        MAX_SIZE = 40,
+        MIN_SIZE = 1,
+            
+        // keys i care about
+        KEYUP = 38,
+        KEYDOWN = 40,
+        ENTER_KEY = 13,
+        
+        FIELDS = {
+            SIZE: "size",
+            MOOD: "mood",
+            SPECIFIC: "specific"
+        },
+        
+        TYPE = "{{type}}",
+        SIZE = "{{size}}",
+        
+        smileyLink,
+        
+        HTML = ["<div class='" + TYPE + "'>",
+                    "\t<div class='left eye'></div>",
+                    "\t<div class='right eye'></div>",
+                    "\t<div class='mouth'></div>",
+                "</div>"].join("\n"),
+                
+        CSS = ["<style type='text/css'>",
+                    "\t.smiley {",
+                    "\t\tfont-size: " + SIZE + "px;",
+                    "\t}",
+               "</style>"].join("\n");
+    
+    
+    var $form, $smiley, $size, $cssCode, $htmlCode;
+    
+    String.prototype.escapeHTML = function () {                                                                                                        
+        return this.replace(/>/g,'&gt;')
+                    .replace(/</g,'&lt;')
+                    .replace(/"/g,'&quot;');
+    };
 
     function addCopyButton($code, text) {
         var $copyButton = $("<span class='copy'/>");
@@ -64,110 +64,122 @@
             }
         });
     }
-	
-	function updateCode() {
-	    var css = CSS.replace(SIZE, $size.val()),
-	        html = HTML.replace(TYPE, $smiley[0].className);
-	    
-		$cssCode.html(css.escapeHTML());
-		addCopyButton($cssCode, css);
-		
-		$htmlCode.html(html.escapeHTML());
-		addCopyButton($htmlCode, html);
-	}
-	
-	function pickSmiley() {
-		var size = $size.val() || DEFAULT_SIZE;
-		
-		$smiley.css("fontSize", size + "px");
+    
+    function updateCode() {
+        var css = CSS.replace(SIZE, $size.val()),
+            html = HTML.replace(TYPE, $smiley[0].className);
+        
+        $cssCode.html(css.escapeHTML());
+        addCopyButton($cssCode, css);
+        
+        $htmlCode.html(html.escapeHTML());
+        addCopyButton($htmlCode, html);
+    }
+    
+    function pickSmiley() {
+        var size = $size.val() || DEFAULT_SIZE;
+        
+        $smiley.css("fontSize", size + "px");
 
-		$smiley.removeClass(ALL_SMILEYS);
-		$smiley.addClass($form.find("select[name=" + FIELDS.MOOD + "]").val());
-		$smiley.addClass($form.find("select[name=" + FIELDS.SPECIFIC + "]").val());
-		
-		updateCode();
-	}
-	
-	function killSubmit(e) {
-		e.preventDefault();
-	}
-	
-	function capSize(size) { // not functioning pun
-		if (isNaN(size)) { return MIN_SIZE; }
-		return Math.max(Math.min(size, MAX_SIZE), MIN_SIZE);
-	}
-	
-	function upAndDown(e) {
-    	var size = parseInt($size.val() || 0, 10);
-    	
+        $smiley.removeClass(ALL_SMILEYS);
+        $smiley.addClass($form.find("select[name=" + FIELDS.MOOD + "]").val());
+        $smiley.addClass($form.find("select[name=" + FIELDS.SPECIFIC + "]").val());
+        
+        updateCode();
+    }
+    
+    function killSubmit(e) {
+        e.preventDefault();
+    }
+    
+    function capSize(size) { // not functioning pun
+        if (isNaN(size)) { return MIN_SIZE; }
+        return Math.max(Math.min(size, MAX_SIZE), MIN_SIZE);
+    }
+    
+    function updateSize(e) {
+        var size = parseInt($size.val() || 0, 10);
+        
         switch (e.keyCode) {
-	        case KEYUP:
-	        	$size.val(capSize(size + 1));
-	        	pickSmiley();
-	            break;
-	            
-	        case KEYDOWN:
-	        	$size.val(capSize(size - 1));
-	        	pickSmiley();
-	            break;
-	            
-	        default:
-	        	break;
+            case KEYUP:
+                $size.val(capSize(size + 1));
+                pickSmiley();
+                break;
+                
+            case KEYDOWN:
+                $size.val(capSize(size - 1));
+                pickSmiley();
+                break;
+                
+            default:
+                break;
         }
-	}
-	
-	var getQueryStringParam = (function() {
-		
-			var query_string = (window.location.search.length > 1) ? window.location.search.substring(1) : null;
-			var pairs = (query_string != null) ? query_string.split("&") : [];
-			var params = {};
-			
-			for (var i=0; i < pairs.length; i++) {
-				var pair = pairs[i].split("=");
-				
-				params[pair[0]] = pair[1] != null ? pair[1] : null;
-			}
-			
-			return function(name) { return params[name] != null ? decodeURIComponent(params[name]) : null; }
-	})();
-	
-	function inititializeSmiley() {
-		var size = getQueryStringParam(FIELDS.SIZE),
-			mood = getQueryStringParam(FIELDS.MOOD),
-			specific = getQueryStringParam(FIELDS.SPECIFIC);
-		
-		if (size) {
-			$size.val(capSize(parseInt(size || 0, 10)));
-		}
-		
-		if (mood) {
-			$form.find("select[name=" + FIELDS.MOOD + "] option[value=" + mood + "]").prop("selected", true);
-		}
-		
-		if (specific) {
-			$form.find("select[name=" + FIELDS.SPECIFIC + "] option[value=" + specific + "]").prop("selected", true);
-		}
-		
-		pickSmiley();
-	}
-	
-	$(function() {
-		$form = $("form.pick-a-smiley");
-		$size = $form.find("input[name=" + FIELDS.SIZE + "]");
-		$smiley = $(".smiley");
-		$cssCode = $("code.css");
-		$htmlCode = $("code.html");
-		
-		var linkHref = $("link[href$='smileys.css']")[0].href;
-		smileyLink = "<link href='" + linkHref + "' type='text/css' rel='stylesheet'>";
-		
-		$form.submit(killSubmit);
-		$form.keydown(upAndDown);
-		
-		$form.find("select[name=" + FIELDS.MOOD + "], select[name=" + FIELDS.SPECIFIC + "]").change(pickSmiley);
-		$size.change(pickSmiley)
-		
-		inititializeSmiley();
-	});
-	
+    }
+
+    function upDownPickSmiley(e) {
+        var keycode = e.keyCode;
+        
+        if (keycode == KEYUP || keycode == KEYDOWN) {
+            pickSmiley();
+        }
+    }
+    
+    var getQueryStringParam = (function() {
+        
+            var query_string = (window.location.search.length > 1) ? window.location.search.substring(1) : null;
+            var pairs = (query_string != null) ? query_string.split("&") : [];
+            var params = {};
+            
+            for (var i=0; i < pairs.length; i++) {
+                var pair = pairs[i].split("=");
+                
+                params[pair[0]] = pair[1] != null ? pair[1] : null;
+            }
+            
+            return function(name) { return params[name] != null ? decodeURIComponent(params[name]) : null; }
+    })();
+    
+    function inititializeSmiley() {
+        var size = getQueryStringParam(FIELDS.SIZE),
+            mood = getQueryStringParam(FIELDS.MOOD),
+            specific = getQueryStringParam(FIELDS.SPECIFIC);
+        
+        if (size) {
+            $size.val(capSize(parseInt(size || 0, 10)));
+        }
+        
+        if (mood) {
+            $form.find("select[name=" + FIELDS.MOOD + "] option[value=" + mood + "]").prop("selected", true);
+        }
+        
+        if (specific) {
+            $form.find("select[name=" + FIELDS.SPECIFIC + "] option[value=" + specific + "]").prop("selected", true);
+        }
+        
+        pickSmiley();
+    }
+    
+    $(function() {
+        $form = $("form.pick-a-smiley");
+        $size = $form.find("input[name=" + FIELDS.SIZE + "]");
+        $smiley = $(".smiley");
+        $cssCode = $("code.css");
+        $htmlCode = $("code.html");
+        
+        var linkHref = $("link[href$='smileys.css']")[0].href;
+        smileyLink = "<link href='" + linkHref + "' type='text/css' rel='stylesheet'>";
+        
+        $form.submit(killSubmit);
+
+        $size
+            .keydown(updateSize)
+            .change(pickSmiley);
+
+        $form.find("select[name=" + FIELDS.MOOD + "], select[name=" + FIELDS.SPECIFIC + "]")
+            .change(pickSmiley)
+            .keyup(upDownPickSmiley);
+        
+        inititializeSmiley();
+    });
+    
 })(this, jQuery);
